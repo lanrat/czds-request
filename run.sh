8c802f4c09183c3b0b05591642816377fbb4e58a
Ian Foster <mrlanrat@gmail.com>
2018-01-28 12:26:41 -0800

    removed dependency on docker-compose

---
+++ run.sh
@@ -0,0 +1,15 @@
+#! /usr/bin/env bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+#echo "starting browser"
+browser_container=$(docker run --rm --name czds-browser -d selenium/standalone-firefox)
+sleep 10
+
+#echo "starting bot"
+docker run --rm --name czds-bot -it --network="container:${browser_container}" -v "${DIR}/config.py:/usr/src/czds-request/config.py:ro" lanrat/czds-request
+
+#echo "stopping browser"
+docker stop ${browser_container} >/dev/null
+
+#echo "done"
