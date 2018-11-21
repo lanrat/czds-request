3172bc386d9802a49793122d6adb9ceb5cb7e964
Ian Foster <mrlanrat@gmail.com>
2018-11-20 18:20:36 -0800

    log to file

--- run.sh
+++ run.sh
@@ -2,13 +2,16 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+DATE=$(date +%F\ %T)
+echo "Running at: $DATE" >> $DIR/request.log
+
 #echo "starting browser"
 #browser_container=$(docker run --rm --name czds-browser -d selenium/standalone-firefox)
 browser_container=$(docker run --rm --name czds-browser -d selenium/standalone-chrome)
-sleep 30
+sleep 20
 
 #echo "starting bot"
-docker run --rm --name czds-bot --network="container:${browser_container}" -v "${DIR}/config.py:/usr/src/czds-request/config.py:ro" lanrat/czds-request
+docker run --rm --name czds-bot --network="container:$browser_container" -v "$DIR/config.py:/usr/src/czds-request/config.py:ro" lanrat/czds-request >> $DIR/request.log
 
 #echo "stopping browser"
 docker stop ${browser_container} >/dev/null
